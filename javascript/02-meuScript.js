fc60920c12f7eef809a531028b63f6b6f95bf0b0
souza_dev <vandersouza2@hotmail.com>
2025-07-30 16:23:18 -0300

    Atribuindo texto a uma variavel e imprimindo
    // Crie uma variavel chamada "uc" e coloque nela o numero da UC que estamos.
    
    // Crie uma variavel chamada "Compatencia" e coloque nela uma frase que represente o que estamos estudando nela.

---
+++ javascript/02-meuScript.js
@@ -0,0 +1,12 @@
+//Exercicios
+
+// Crie uma variavel chamada "uc" e coloque nela o numero da UC que estamos.
+
+// Crie uma variavel chamada "Compatencia" e coloque nela uma frase que represente o que estamos estudando nela.
+
+
+let uc = 2;
+
+let compatencia = "estamos apreendendo a estilização de paginas com css e a utilização da liguagem JavaScript`";
+
+console.log(`Nesta UC ${uc} ${compatencia}` );
