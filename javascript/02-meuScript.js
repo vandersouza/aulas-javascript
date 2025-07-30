4f4764f5e67f9586ba4fe84a634426e185126f3a
souza_dev <vandersouza2@hotmail.com>
2025-07-30 16:40:02 -0300

    atribuindo aspas a saide de textos

--- javascript/02-meuScript.js
+++ javascript/02-meuScript.js
@@ -9,4 +9,15 @@ let uc = 2;
 
 let compatencia = "estamos apreendendo a estilização de paginas com css e a utilização da liguagem JavaScript`";
 
+
+// usando crases para mostrar variaveis junto com texto
 console.log(`Nesta UC ${uc} ${compatencia}` );
+
+// as duplas comando
+console.log("Gosto muito de paçoca");
+
+//aspas duplas como texto
+console.log('Gosto "muito" de paçoca');
+// onde estiver barra invertida é somente texto scape de texto
+console.log('Gosto \"muito\" de paçoca');
+
