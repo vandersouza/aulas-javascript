f33827d870954f8a143e8b9061ff38c49b5a8141
souza_dev <vandersouza2@hotmail.com>
2025-08-19 14:56:23 -0300

    usando else
    utilizando else na saida

--- javascript/05-condicionais.js
+++ javascript/05-condicionais.js
@@ -11,11 +11,37 @@ console.log("Exemplo 1");
 
 let numero = 5;
 
-if(numero >= 10){
+if (numero >= 10) {
 
     console.log("Se voce esta bvendo essa mensagem é porque a condicao é verdadeira!");
 
 };
+
+console.log("\nExemplo 2");
+
+
+let aluno = "joao";
+let idade = 15;
+
+
+if (idade < 18) {
+
+    console.log("aluno é menor de idade");
+
+}
+
+else {
+
+    console.log("maior de idade");
+
+}
+
+
+
+
+
+
+
 
 
 
