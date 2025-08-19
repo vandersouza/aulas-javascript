69ee2e3b0e564390695bb9c10c8f450f25e28caf
souza_dev <vandersouza2@hotmail.com>
2025-08-19 15:18:41 -0300

    usando_if avançado

--- javascript/05-condicionais.js
+++ javascript/05-condicionais.js
@@ -37,6 +37,44 @@ else {
 }
 
 
+console.log("\nExercicio");
+
+
+/*1. Crie duas variáveis conforme a seguir:
+ 
+- Nota 1 (contendo um valor de 0 a 10)
+- Nota 2 (contendo outro valor de 0 a 10)
+ 
+2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2.
+ 
+3. Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+Caso contrário, mostre "reprovado".*/
+
+
+
+
+let nota1 = 3;
+let nota2 = 2;
+
+let media = nota1 + nota2 / 2;
+
+if (media >= 7) {
+
+    console.log("Aluno aprovado");
+}
+
+if (media == 5) {
+
+    console.log("Aluno de Recuperação");
+
+}
+
+if (media < 5) {
+
+    console.log("Aluno reprovado");
+
+}
 
 
 
