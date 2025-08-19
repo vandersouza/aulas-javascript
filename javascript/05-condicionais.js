76e6202fa27500959e5cdebad2325d2a24a7e17e
souza_dev <vandersouza2@hotmail.com>
2025-08-19 14:37:13 -0300

    utilizando condicionais
    usando if

---
+++ javascript/05-condicionais.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*Comandos condicionais mais comuns
+
+if -> SE
+
+else -> SENÃO
+*/
+
+console.log("Exemplo 1");
+
+let numero = 5;
+
+if(numero >= 10){
+
+    console.log("Se voce esta bvendo essa mensagem é porque a condicao é verdadeira!");
+
+};
+
+
+
+
+
