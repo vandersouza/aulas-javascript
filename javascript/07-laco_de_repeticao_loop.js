f6399bacb2f18f6698d51ccdee8b5a3219561a09
souza_dev <vandersouza2@hotmail.com>
2025-08-20 13:57:29 -0300

    Conseito sobre Loops
    Conceitos como funciona laços de repetição

---
+++ javascript/07-laco_de_repeticao_loop.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* loops ou estrturas de reptição
+
+São Comandos que, baseados em algumas condição logica são programados para executar ações repetidas vezes.
+
+Comando mais tradicionais: while (Enquanto) e for (para)
+
+Normalmente os loops são controlados atraves de uma variavel contadora
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
