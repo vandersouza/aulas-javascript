4845e2dde9bca32983b8ff79bbd3af5f0dc6e9c8
souza_dev <vandersouza2@hotmail.com>
2025-08-20 14:59:47 -0300

    Usando for com array
    usando acesso array com for tradiconal

--- javascript/07-laco_de_repeticao_loop.js
+++ javascript/07-laco_de_repeticao_loop.js
@@ -12,14 +12,66 @@ Normalmente os loops são controlados atraves de uma variavel contadora
 
 console.log("Exemplo 1: While (enquanto)")
 
-let contador = 1;
+let i = 1;
 
-while( contador <= 5){
+while( i <= 5){
 
-console.log("passagem do loop"+contador);
-contador++;
+
+/*Incremento como funciona i = i + 1
+
+i++; padrão de uso
+
+i+= 2; conta de dois em dois
+
+*/
+
+console.log("passagem do loop"+i);
+i++;
 
 }
+
+
+
+
+
+console.log("\nExemplo 2: for (para)");
+
+/*For (inicialização de variavel condição e atualização)
+
+decremento
+for(let j = 50; j >= 1; j--)
+
+Incremento
+for(let j = 1; j <= 10; j++)
+{
+
+*/
+
+for(let j = 1; j <= 10; j++){
+
+
+
+    console.log("J vale agora"+j);
+}
+
+console.log("\nExemplo 4: loop e array");
+
+
+const bandas = ["maria","joao","pedro","joana","teresa","jacinto"];
+
+for(let i = 0; i <= bandas.length; i++ ){
+
+    console.log(i +bandas[i]);
+    
+
+
+}
+
+
+
+
+
+
 
 
 
