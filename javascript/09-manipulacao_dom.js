d66de4fe1df1498c0db48e1e54be82f8f5edc923
souza_dev <vandersouza2@hotmail.com>
2025-08-26 17:26:58 -0300

    inicio de Dom

---
+++ javascript/09-manipulacao_dom.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*Exemplo 01*/
+
+//Procurando (querySelector) por um elemento chamado exemplo1
+const exemplo1 = document.querySelector("#exemplo1");
+const mensagem1 = document.querySelector("#mensagem01");
+const corpo = document.querySelector("body");
+
+console.log(exemplo1);
+console.log(mensagem1);
+console.log(corpo);
+
+
+
+
+
