75087c5b9bde9da88ff5941c1f7161b7eb817232
souza_dev <vandersouza2@hotmail.com>
2025-09-03 16:31:10 -0300

    validando senha e mostrar botao

---
+++ javascript/10-senha.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+/*variavel que recebe o campo senha */
+const campoSenha = document.querySelector("#senha");
+
+/*variavel usada recebe o botão mostrar */
+const btnMostrar = document.querySelector("#mostrar");
+
+/*declarando função para receber evento do botão presionado */
+btnMostrar.addEventListener("pointerdown", function(){
+
+    campoSenha.type = "text";
+});
+
+/*declarando função para receber evento do botão soltar */
+btnMostrar.addEventListener("pointerup", function(){
+
+campoSenha.type = "password";
+
+});
+
+btnMostrar.addEventListener("pointerout", function(){
+
+
+campoSenha.type ="password"
+
+});
